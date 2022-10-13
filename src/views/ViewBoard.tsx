56169e86367cffd96e3966a0eac759a85e6c1100
Joan Gerard <joan.gerard@outlook.com>
2022-10-13 20:20:27 +0200

    implement logic to fetch a single board

--- src/views/ViewBoard.tsx
+++ src/views/ViewBoard.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import API from "../utils/API";
 
 const ViewBoard = () => {
-  return (
-    <div>ViewBoard</div>
-  )
-}
+  const params = useParams(); // inside your component
+  const boardId = params.id;
 
-export default ViewBoard
+  const [boardData, setBoardData] = useState<BoardData>();
+
+  console.log({ boardId });
+
+  const getBoard = async () => {
+    const res = await API.get<BoardData>({ path: `/boards/${boardId}` });
+    console.log("get board response", res);
+    setBoardData(res);
+  };
+
+  useEffect(() => {
+    getBoard();
+  }, []);
+
+  return (
+    <div>
+      <p>{boardData?.boardName}</p>
+      <p>{boardData?.description}</p>
+      <div>
+        {boardData?.ideas.map((idea) => (
+          <div>
+            <h3>{idea.ideaTitle}</h3>
+            <p>{idea.description}</p>
+            <p>Votes = {idea.votes}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ViewBoard;
